4f6e02427fc98d36a7f016eed32653fd227ee864
root <root@ip-172-31-22-7.us-west-2.compute.internal>
2016-05-12 06:36:48 +0000

    a

--- code/2.py
+++ code/2.py
@@ -40,17 +40,19 @@ def set_parameters(NoJ):
     random.seed(0)
     Parameters = []
     SubmittingTime = [2000]
-    SubmittingStandardInterval1 = 1000
-    SubmittingStandardInterval2 = 2000
+    SubmittingStandardInterval1 = 3000
+    SubmittingStandardInterval2 = 8000
     cT = [0]
     TaskRunTimes = []
     JobSizes = []
     for i in range(NoJ):
-        TaskRunTimes.append(random.randint(3000,5000))
-        JobSizes.append(random.randint(5,5))
-        cT.append(cT[-1] + TaskRunTimes[-1]*Sizes[-1])
+        TaskRunTimes.append(4000)
+        JobSizes.append(20)
+#        TaskRunTimes.append(random.randint(3000,5000))
+#        JobSizes.append(random.randint(5,5))
+#        cT.append(cT[-1] + TaskRunTimes[-1]*Sizes[-1])
     for i in range(NoJ-1):
-	if i < 20:
+	if i < 4:
        	    SubmittingTime.append(SubmittingTime[-1] + int(random.random()*SubmittingStandardInterval1))
     	else:
        	    SubmittingTime.append(SubmittingTime[-1] + int(random.random()*SubmittingStandardInterval2))
@@ -58,13 +60,16 @@ def set_parameters(NoJ):
         if i==2:
 #        if i%10==2:
 #            Parameters.append([i, SubmittingTime[i], random.randint(11000,15000), random.randint(11, 40)])
-            Parameters.append([i, SubmittingTime[i], random.randint(6000,6000), random.randint(20, 20)])
+#            Parameters.append([i, SubmittingTime[i], random.randint(6000,6000), random.randint(20, 20)])
+            Parameters.append([i, SubmittingTime[i], 5000, 20]) 
         else:
-            Parameters.append([i, SubmittingTime[i], random.randint(1000,5000), random.randint(1,10)]) 
+            Parameters.append([i, SubmittingTime[i], TaskRunTimes[i], JobSizes[i]])
+#            Parameters.append([i, SubmittingTime[i], random.randint(1000,5000), random.randint(1,10)]) 
+#            Parameters.append([i, SubmittingTime[i], TaskRunTimes[i], JobSizes[i]])
     return Parameters
 
 if __name__=="__main__":
-    NoJ = 200
+    NoJ = 100
     pool = ThreadPool(NoJ)
     parameters = set_parameters(NoJ)
 
@@ -73,9 +78,11 @@ if __name__=="__main__":
     for i in range(NoJ):
 	if i > 0:
 	    jobProperties = jobProperties + " "
-	jobProperties = jobProperties + str(parameters[i][0]) + "+" + str(parameters[i][1]) + "+" + str(parameters[i][2]*parameters[i][3])
+	jobProperties = jobProperties + str(parameters[i][0]) + "+" + str(parameters[i][1]) + "+" + str(int(parameters[i][2]*parameters[i][3]/20.0))
 
     print "jobProperties: "+jobProperties
+    f=file("/root/spark/job.profiledInfo","w")
+    f.write(jobProperties)
 
     pool.map(run_job, parameters)
     pool.close()

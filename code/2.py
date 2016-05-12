ac8820ea98d76754318a7c4606813072165cd41c
Chen Chen <chenc10@126.com>
2016-05-12 13:51:02 +0800

    modify micro-starvation-gps.py

--- code/2.py
+++ code/2.py
@@ -37,25 +37,28 @@ def run_job(P):
     print "job-%d finishes" %P[0]
 
 def set_parameters(NoJ):
-    print "hah"
     random.seed(0)
     Parameters = []
     SubmittingTime = [2000]
-    SubmittingStandardInterval = 2000
+    SubmittingStandardInterval1 = 1000
+    SubmittingStandardInterval2 = 2000
     cT = [0]
     TaskRunTimes = []
     JobSizes = []
     for i in range(NoJ):
-        TaskRunTimes.append(random.randint(1000,5000)
-#        JobSizes.append(random.randint(5,5))
+        TaskRunTimes.append(random.randint(3000,5000))
+        JobSizes.append(random.randint(5,5))
         cT.append(cT[-1] + TaskRunTimes[-1]*Sizes[-1])
     for i in range(NoJ-1):
-       SubmittingTime.append(SubmittingTime[-1] + int(random.random()*SubmittingStandardInterval))
+	if i < 20:
+       	    SubmittingTime.append(SubmittingTime[-1] + int(random.random()*SubmittingStandardInterval1))
+    	else:
+       	    SubmittingTime.append(SubmittingTime[-1] + int(random.random()*SubmittingStandardInterval2))
     for i in range(NoJ):
         if i==2:
 #        if i%10==2:
 #            Parameters.append([i, SubmittingTime[i], random.randint(11000,15000), random.randint(11, 40)])
-            Parameters.append([i, SubmittingTime[i], random.randint(11000,15000), random.randint(20, 20)])
+            Parameters.append([i, SubmittingTime[i], random.randint(6000,6000), random.randint(20, 20)])
         else:
             Parameters.append([i, SubmittingTime[i], random.randint(1000,5000), random.randint(1,10)]) 
     return Parameters
